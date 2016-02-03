45cbaf30a9a524b19b1e4071922f7ebceafe8db4
josephmanis <joseph@josephs-macbook-pro.local>
2016-02-03 00:58:15 -0500

    Initial Commit

---
+++ EvilMusicPlayerTests/EvilMusicPlayerTests.m
@@ -0,0 +1,39 @@
+//
+//  EvilMusicPlayerTests.m
+//  EvilMusicPlayerTests
+//
+//  Created by Joseph Manis on 2/3/16.
+//  Copyright © 2016 Joseph Manis. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EvilMusicPlayerTests : XCTestCase
+
+@end
+
+@implementation EvilMusicPlayerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
